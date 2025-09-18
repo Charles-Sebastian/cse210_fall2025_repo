f1f23e4950e421ee91d093dfcfcbcb43a7541fbf
Charles-Sebastian <charles06@byui.edu>
2025-09-18 14:27:53 -0600

    Complete the second assignment due on 09_18_25 - letter grade calculator and pass or fail calculator

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        int gradePercentage = int.Parse(Console.ReadLine());
+
+        string gradeLetter;
+
+        if (gradePercentage >= 90)
+        {
+            gradeLetter = "A"; ;
+        }
+        else if (gradePercentage >= 80)
+        {
+            gradeLetter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            gradeLetter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            gradeLetter = "D";
+        }
+        else
+        {
+            gradeLetter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {gradeLetter}.");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("You passed!!! Congratulations!!!!");
+        }
+        else
+        {
+            Console.WriteLine("You failed. Work hard and you'll do better next time");
+        }
+
     }
 }

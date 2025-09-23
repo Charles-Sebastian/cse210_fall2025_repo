a87158ab50d31af92a3c7c356e815a9dcde31c4f
Charles-Sebastian <charles06@byui.edu>
2025-09-23 12:56:06 -0600

    Completed Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,100);
+
+        Console.WriteLine($"What is the magic number? {magicNumber}");
+
+        int userGuess;
+
+        do
+        {
+            Console.Write($"What is your guess? ");
+            userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess == magicNumber)
+            {
+                Console.WriteLine("You guessed it!!");
+            }
+            else if (userGuess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Higher");
+            }
+        } while (userGuess != magicNumber);
     }
 }

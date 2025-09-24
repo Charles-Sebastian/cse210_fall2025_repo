2134efe46c4295e21761061da69c8693da52796e
Charles-Sebastian <charles06@byui.edu>
2025-09-24 13:07:31 -0600

    Completed Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.Serialization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        bool collectData = true;
+        int sum = 0;
+        int greatestNumber = 0;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.Write("Enter number: ");
+            int userNumber = int.Parse(Console.ReadLine());
+
+            if (userNumber == 0)
+            {
+                collectData = false;
+            }
+            else
+            {
+                numbers.Add(userNumber);
+            }
+
+            if (userNumber > greatestNumber)
+            {
+                greatestNumber = userNumber;
+            }
+        } while (collectData);
+
+        foreach (int number in numbers)
+        {
+            sum = sum + number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {sum / numbers.Count}");
+        Console.WriteLine($"The largest number is: {greatestNumber}");
     }
 }
